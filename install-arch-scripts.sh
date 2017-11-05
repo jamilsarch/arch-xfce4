2ec56b9900743b3b91b2f6cbe82f8d4e2a870724
Jamil Akhtar <jamil@jamsarch.localdomain>
2017-11-05 16:37:40 +0000

    xfce4

--- install-arch-scripts.sh
+++ install-arch-scripts.sh
@@ -6,21 +6,20 @@
 
 sudo pacman -Syyu
 sudo pacman -S xorg
-sudo pacman -S xfce4 xfce4-goodies  
-sudo pacman -S xterm xorg-xclock xorg-twm xorg-xinit xorg-server
+sudo pacman -S xfce4 xfce4-goodies xterm xorg-xclock xorg-twm xorg-xinit xorg-server
 sudo pacman -S lightdm lightdm-gtk-greeter accountsservice
 sudo systemctl enable lightdm
 sudo systemctl enable accounts-daemon
 sudo useradd -m -g users -G wheel -s /bin/bash jamil
 passwd jamil
-sudo pacman -S thunar-meditags-plugin gvfs file-roller xdg-user-dirs
-sudo pacman -S cmatrix terminator qbittorrent gufw ufw linux-kernel pulseaudio pulseaudio-alsa pavucontrol
-sudo pacman -S screenfetch simple-scan firefox opera geary plank libreoffice-fresh
-sudo pacman -S nvidia nvidia-settings 
-sudo nvidia-xconfig
-sudo pacman -S bluez bluez-utils pulseaudio-bluetooth bluez-utils bluez-libs gnome-bluetooth blueberry bluez-tools
+sudo pacman -S thunar-meditags-plugin gvfs file-roller xdg-user-dirs cmatrix terminator qbittorrent gufw ufw linux-headers pulseaudio pulseaudio-alsa 
+sudo pacman -S screenfetch simple-scan firefox opera geary plank libreoffice-fresh pavucontrol nvidia nvidia-settings powerline2 python2-powerline    
+sudo pacman -S bluez bluez-utils pulseaudio-bluetooth bluez-utils bluez-libs gnome-bluetooth blueberry bluez-tools zsh vim virtualbox
 sudo systemctl enable bluetooth.service
 sudo systemctl start bluetooth.service
+sudo nvidia-xconfig
+
+
 
 
 
